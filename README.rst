a07ebc6417cdd697461e35235e4edba9220abb90
ZeeD26 <zeed.dev@icloud.com>
2016-10-01 16:39:12 +0200

    Prepare for pypi.

---
+++ README.rst
@@ -0,0 +1,5 @@
+============
+ColorizedLog
+============
+This package provides a replacement for Python's builtin StreamHandler to add
+color to each message depending on its log level.
--- setup.py
+++ setup.py
@@ -11,12 +11,28 @@ _version_re = re.compile(r'__version__\s+=\s+(.*)')
 with open(os.path.join(root_dir, 'colorizedlog.py'), 'r') as f:
     version = str(ast.literal_eval(_version_re.search(f.read()).group(1)))
 
+description = "Replacement for logging.StreamHandler with colors."
+
 setup(
     name='colorizedlog',
     version=version,
+    description=description,
+    long_description=long_description,
     author='Dominik Steinberger',
     url='https://github.com/ZeeD26/colorizedlog',
     py_modules=[
         'colorizedlog'
     ],
-    license='BSD')
+    license='BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: System :: Logging'
+    ])
